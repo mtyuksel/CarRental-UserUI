e58f9600578088ad62c0a67a52de04da0347bbbc
mtyuksel <yukselmtevfik@gmail.com>
2021-06-01 21:34:05 +0300

    LocationService API url fix.

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Location } from 'app/models/location';
 import { ListResponseModel } from 'app/models/response_models/listResponseModel';
 import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
@@ -9,11 +10,11 @@ import { Observable } from 'rxjs';
 })
 export class LocationService {
 
-  apiUrl: string = environment.api_base_url + "locations";
+  private apiUrl: string = environment.api_base_url + "locations";
 
   constructor(private httpClient: HttpClient) { }
 
   getLocations(): Observable<ListResponseModel<Location>> {
-    return this.httpClient.get<ListResponseModel<Location>>(this.apiUrl + "get");
+    return this.httpClient.get<ListResponseModel<Location>>(this.apiUrl + "/getall");
   }
 }
