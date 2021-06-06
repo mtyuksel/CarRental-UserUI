b1dff1f7243decd460ddc8ff6b3686fad0055a06
mtyuksel <yukselmtevfik@gmail.com>
2021-06-06 16:46:31 +0300

    CarDetailDto added.

--- src/app/models/DTOs/carDetailDto.ts
+++ src/app/models/DTOs/carDetailDto.ts
@@ -1,14 +1,16 @@
 import { Brand } from "../brand";
 import { Color } from "../color";
+import { Image } from "../image";
 import { LocationDto } from "./locationDto";
 
-export interface CarDetailDto{
-    id:number,
-    name:string,
-    brand:Brand,
-    color:Color,
-    location:LocationDto,
-    modelYear:number,
-    dailyPrice:number,
-    description:string
+export interface CarDetailDto {
+    id: number,
+    name: string,
+    brand: Brand,
+    color: Color,
+    location: LocationDto,
+    modelYear: number,
+    dailyPrice: number,
+    description: string,
+    images: Image[]
 }
